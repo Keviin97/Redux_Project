c97b7b34aa40190a17e61d8d0477096d16674d1c
Keviin97 <kevinxela13@gmail.com>
2019-09-04 16:51:01 -0600

    Uso del archivo api

--- redux/actions/postActions.js
+++ redux/actions/postActions.js
@@ -1,3 +1,5 @@
+import { api } from '../../utility/api';
+
 const request = require('superagent');
 const token = 'Token bf6ab47d00c071af3f209663fc6c20aec2241927';
 
@@ -12,16 +14,16 @@ export const setData = (type,data) => ({
 export function listPosts() {
   const path = 'http://localhost:9000/api/post/'
 
-  return (dispatch) => {
-    return request.get(path).set('Accept', 'application/json').set('Content-Type', 'application/json').set('Authorization', token)
-      .then((res) => {
-        dispatch(setData(LIST_POSTS,res.body))
-        console.log('Datos listados')
-      })
-      .catch((error) => {
-        console.log(error)
-        console.log('Error en la peticion')
-      })
+  return(dispatch) => {
+    return api.get(path)
+    .then((res) => {
+      dispatch(setData(LIST_POSTS,res.body))
+      console.log('Datos listados')
+    })
+    .catch((error) => {
+      console.log(error)
+      console.log('Error en la peticion')
+    })
   }
 }
 
--- utility/api.js
+++ utility/api.js
@@ -1,8 +1,8 @@
 const request = require('superagent');
 const token = 'Token bf6ab47d00c071af3f209663fc6c20aec2241927';
-const path = 'http://localhost:9000/post/'
+// const path = 'http://localhost:9000/post/'
 
-function get() {
+function get(path) {
   return request.get(path).
     set('Accept', 'application/json').set('Content-Type', 'application/json').set('Authorization', token);
 }
